74764603770ea5d065aa91592c4bd35b4afb084e
Cody Fizette <cfizette@pop-os.localdomain>
2019-06-25 00:06:12 -0400

    successfully uploaded and captioned image using azure function

--- azure_api/CaptionSpeakFunction/HttpTrigger/__init__.py
+++ azure_api/CaptionSpeakFunction/HttpTrigger/__init__.py
@@ -1,18 +1,27 @@
 import logging
 import io
 import azure.functions as func
+from HttpTrigger.azure_helpers import AzureCaptioner
+import base64
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
+    # Body of request must contain b64 encoded image data
+
     logging.info('Python HTTP trigger function processed a request.')
 
-    body = req.get_body()
-    #body = io.BytesIO(body)
-    if not body:
+    # Read, decode, and construct generator
+    image_encoded = req.get_body()
+    image_decoded = base64.b64decode(image_encoded)
+    image = io.BytesIO(image_decoded)
+
+    if not image:
         pass
 
-    if body:
-        return func.HttpResponse('{}'.format(str(body)))
+    if image:
+        captioner = AzureCaptioner()
+        caption = captioner.generate_caption(image)
+        return func.HttpResponse('{}'.format(caption))
     else:
         return func.HttpResponse(
              "Please pass a name on the query string or in the request body",
--- azure_api/request_test.py
+++ azure_api/request_test.py
@@ -1,11 +1,13 @@
 import requests
 import io
+import base64
 
 
-with open('foo.txt') as f:
+with open('cat.jpg', 'rb') as f:
     data = f.read()
-    r = requests.get('http://localhost:7071/api/HttpTrigger', data=data)
-
-
+    data = base64.b64encode(data)  # data must be b64 encoded to be read correctly by azure function
+    print(data[:10])
+    print(len(data))
+    r = requests.post('http://localhost:7071/api/HttpTrigger', data)
 
 print(r.text)
