a08b48eebb5b4b2853d459418eed8438cd74da49
Cody Fizette <cfizette@pop-os.localdomain>
2019-06-30 20:19:12 -0400

    Added camera shutter sound when photo taken

--- camera_app/cam.py
+++ camera_app/cam.py
@@ -23,6 +23,7 @@ WAIT_AFTER_PHOTO = 3
 IMG_DIR = 'photos'
 AUDIO_FREQUENCY = 12000 #TODO figure out correct setting for this
 REQUEST_URL = 'https://seeing-eye-pi.azure-api.net/ImageToSpeechV1/HttpTrigger'
+SHUTTER_EFFECT = 'assets/camera-shutter.ogg'
 # 12000 sounds good on my computer but I have to increase it to sound correct on the Raspberry Pi
 
 
@@ -65,6 +66,7 @@ class CameraApp:
         self.camera = PiCamera()
         self.filesystem = CameraAppFilesystem()
         self.take_photo_button = GPIOButton(TAKE_PHOTO_PIN)
+        self.shutter_effect = pygame.mixer.Sound(SHUTTER_EFFECT)
 
     def show_viewfinder(self):
         stream = self.capture_photo_to_stream(use_video_port=True, resize=WINDOW_SIZE, img_format='rgb')
@@ -84,22 +86,23 @@ class CameraApp:
         stream.seek(0)
         return stream
 
-    def play_audio(self, audio):
-        sound = pygame.mixer.Sound(audio)
+    def play_pygame_sound(self, sound):
         channel = sound.play()
         # Need to pause to allow sound to be played
         while channel.get_busy():
-            pygame.time.delay(100)
+            pygame.time.delay(10)
 
     def capture_and_process_image(self):
         print('Taking photo in capture_and_process_image')
+        self.play_pygame_sound(self.shutter_effect)
         stream = self.capture_photo_to_stream(use_video_port=False, resize=FULL_IMAGE_SIZE, img_format='jpeg')
         image_bytes = stream.read()
         print('Sending photo to Azure')
         audio = self.image_to_speech.post_request(image_bytes)
         print("Received audio")
         print('Playing audio')
-        self.play_audio(audio)
+        sound = pygame.mixer.Sound(audio)
+        self.play_pygame_sound(sound)
         print("Saving data")
         self.filesystem.save_img_and_caption(stream, 'foo')  # TODO add caption here
 
