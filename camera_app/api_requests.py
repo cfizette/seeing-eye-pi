776918f07a153d75421cb5d658c0fb23e49af410
Cody Fizette <cfizette@pop-os.localdomain>
2019-07-01 23:33:13 -0400

    Added docstrings to api_requests

--- camera_app/api_requests.py
+++ camera_app/api_requests.py
@@ -1,17 +1,25 @@
-import os
-import subprocess
 import requests
-import time
-import pdb
-import io
 import base64
 import json
+from typing import Tuple
 
 class ImageToSpeechRequest(object):
     def __init__(self, url):
         self.url = url
     
-    def post_request(self, image: bytes) -> str:
+    def post_request(self, image: bytes) -> Tuple[str, bytes]:
+        """Post image and receive response.
+        Image data sent in request body.
+        Response contains the following:
+            Headers: {'caption': String of caption describing processed image}
+            Body: Base64 encoded audio of the caption
+        
+        Arguments:
+            image {bytes} -- Image to be posted.
+        
+        Returns:
+            Tuple[str, bytes] -- Caption and decoded audio.
+        """
         image_encoded = base64.b64encode(image)
         response = requests.post(self.url, image_encoded) 
         caption = response.headers['caption']
